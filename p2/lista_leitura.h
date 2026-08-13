e860196ef1ea532387aa44f71c102b912f3c07ec
Erickuri-sudo <erick12.santos60@gmail.com>
2026-08-13 12:35:27 -0300

    fix: remove os exercícios já feitos do repo

--- p2/lista_leitura.h
+++
@@ -1,8 +0,0 @@
-#ifndef lista_leitura_h
-#define lista_leitura_h
-#include "leitura.h"
-typedef struct lista_leitura tLista_leitura;
-
-tLista_leitura *criaListaLeitura();
-void insereListaLeitura(tLista_leitura *lista,tLeitura *leitura);
-#endif
