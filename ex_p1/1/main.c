32c9c6a433a39d5d60e561d0d6766a06c67c137f
Erickuri-sudo <erick12.santos60@gmail.com>
2026-09-08 12:05:03 -0300

    main do exercicio 1

---
+++ ex_p1/1/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "mapa.h"
+
+int main(int argc, char const *argv[])
+{
+    freopen("entrada.txt","r",stdin);
+    freopen("saida.txt","w",stdout);
+    int lins = 0,cols = 0;
+
+    scanf("%d %d",&lins,&cols);
+
+    Mapa* mapa = criaMapa(lins,cols);
+    for(int i = 0;i<lins;i++){
+        for(int j = 0; j < cols;j++){
+            char buffer[15];
+            scanf("%14s",buffer);
+            insereNomeMapa(mapa,buffer,i,j);
+        }
+    }
+    Mapa* trs = transpoe(mapa);
+
+    imprimeMapa(mapa);
+    printf("\n");
+    printf("== Primeiro experimento:\n");
+    imprimeMapa(trs);
+    
+    return 0;
+}
